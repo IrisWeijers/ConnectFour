922737ef86b5d346eef30616e8cf9dfe9d12c704
IrisWeijers <i.weijers@student.utwente.nl>
2017-01-16 11:48:44 +0100

    pseudocode Game

--- src/ConnectFour/Game.java
+++ src/ConnectFour/Game.java
@@ -1,5 +1,60 @@
 package ConnectFour;
 
 public class Game {
+  /**
+  list of players
+  */
+  private Player[] players;
+  
+  private static board;
+  
+  private int currentPlayer;
+  
+ /**
+ creates a new game object with players
+ */
+  public Game(Player p1, Player p2){
+  }
+  
+  /**
+  starts the game
+  Asks after each ended game if the user want to continue. Continues until the user does not want to play anymore.
+  */
+  public void Start(){
+  }
+  
+  /**
+  resets the game.
+  The board is emptied and player[0] becomes the current player.
+  */
+  private void reset(){
+  }
+  
+  /**
+   Plays the Connet Four game.
+   First the (still empty) board is shown. Then the game is played until it
+   is over. Players can make a move one after the other. After each move,
+   the changed game situation is printed.
+  */
+  private void play(){
+  }
+  
+  /**
+  ??
+  */
+  private boolean readBoolean(String promt, String yes, String no){
+  }
+  
+  /**
+  print game situation
+  */
+  private void update(){
+  }
+  
+  /**
+  prints the game results of last game
+  */
+  private void printResult(){
+  }
 
 }
