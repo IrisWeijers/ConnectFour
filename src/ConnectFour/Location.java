5ce938b39ac8ca27ac4b1e7f336ea46d4b271699
IrisWeijers <i.weijers@student.utwente.nl>
2017-01-16 11:38:02 +0100

    pseudocode Location

--- src/ConnectFour/Location.java
+++ src/ConnectFour/Location.java
@@ -2,4 +2,57 @@ package ConnectFour;
 
 public class Location {
 
+  private int x;
+  private int y;
+  private int z;
+  
+  public Location (int x, int y, int z){
+  }
+  
+  /**
+  No clue what to do with this
+  */
+  public int compareTo(Location argument){
+  }
+  
+  /**
+  searches for the x coördinate of the location
+  */
+  pubic int getX(){
+  }
+  /**
+  searches for the y coördinate of the location
+  */
+  public int getY(){
+  }
+  
+  /**
+  searches for the z coördinate of the location
+  */
+  public int getZ(){
+  }
+  
+  /**
+  set the x cooirdinate of the location
+  */
+  public void setX(int x){
+  }
+  
+   /**
+  set the y cooirdinate of the location
+  */
+  public void setY(int Y){
+  }
+  
+   /*
+  set the z cooirdinate of the location
+  */
+  public void setZ(int z){
+  }
+  
+  /**
+  make a String of the location
+  */
+  public String toString(){
+  }
 }
