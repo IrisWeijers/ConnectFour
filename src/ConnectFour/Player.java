1d9dda6dc3bc1e201577b67697a8b1ab0a810d25
IrisWeijers <i.weijers@student.utwente.nl>
2017-01-16 12:24:05 +0100

    pseudocode player

--- src/ConnectFour/Player.java
+++ src/ConnectFour/Player.java
@@ -1,5 +1,55 @@
 package ConnectFour;
 
+/**
+Abstract class for keeping a player in the Connect Four game.
+*/
 public abstract class Player {
+
+/**
+player's name
+*/
+ private String name;
+
+/**
+player's color
+*/
+ private LocationState ls;
+
+  /**
+Creates a new Player object
+*/  
+  public Player player(String name, LocationState ls){
+  }
+  
+  /**
+  get name of player
+  */
+  public String getName() {
+    return name;
+  }
+  
+  /**
+  get playing color of player
+  */
+  public LocationState getLocationState() {
+        return ls;
+    
+  /**
+   Determines the field for the next move.
+   
+  @param board
+             the current game board
+  @return the player's choice
+  */
+  public abstract int determineMove(Board board);
+   
+    /**
+    Makes a move on the board.
+    @parameter board
+    */
+  public void makeMove(Board board) {
+  }
+    
+    
 
 }
