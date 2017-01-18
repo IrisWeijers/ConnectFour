b47a6fededc67624b3df8587dc2f829496405b7c
SamOye95 <a.s.m.oyediran@student.utwente.nl>
2017-01-18 14:42:48 +0100

    Update HumanPlayer.java

--- src/ConnectFour/HumanPlayer.java
+++ src/ConnectFour/HumanPlayer.java
@@ -1,5 +1,28 @@
 package ConnectFour;
 
+import java.util.Scanner;
+/**
+ * This class extends IPlayer and this human player
+ * makes their moves by user input 
+ * via the console or the GUI
+ * @author Sam
+ */
 public class HumanPlayer {
+  
+  public HumanPlayer(LocationState playerState) {
+  
+  }
 
+  /**
+	 * Move which returns the players chosen column
+	 */
+  @Override
+  
+  public int getMove(Board board) {
+    
+    
+  }
+  
+  
+  
 }
