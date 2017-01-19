4543693e4e70684f897fb8d9e729e1c48b4ea510
Samuel Oyediran <a.s.m.oyediran@student.utwente.nl>
2017-01-19 18:59:22 +0100

    Updated Location

--- src/ConnectFour/Location.java
+++ src/ConnectFour/Location.java
@@ -1,6 +1,6 @@
 package ConnectFour;
 
-public class Location {
+public class Location implements Comparable<Location> {
 
   private int x;
   private int y;
@@ -9,31 +9,64 @@ public class Location {
   /**
   create location with x, y and z coördinates
   */
-  public Location location(int x, int y, int z){
+  public Location (int x, int y, int z){
+	  super();
+	  this.x = x;
+	  this.y = y;
+	  this.z = z;
   }
   
   /**
   ??
   @parameter argmument
   */
-  public int compareTo(Location argument){
-  }
-  
+  public int compareTo(Location arguement) {
+		if (this.x < arguement.x)
+			return -1;
+		if (this.x == arguement.x && this.y < arguement.y)
+			return -1;
+		if (this.x == arguement.x && this.y > arguement.y)
+			return +1;
+		if (this.x > arguement.x)
+			return +1;
+		if (this.y < arguement.z)
+			return -1;
+		if (this.y == arguement.y && this.z < arguement.z)
+			return -1;
+		if (this.y == arguement.y && this.z > arguement.z)
+			return +1;
+		if (this.y > arguement.y)
+			return +1;
+		if (this.z < arguement.z)
+			return -1;
+		if (this.z == arguement.z && this.x < arguement.x)
+			return -1;
+		if (this.z == arguement.z && this.x > arguement.x)
+			return +1;
+		if (this.z > arguement.z)
+			return +1;
+		else
+			return 0;
+	}
+
   /**
   searches for the x coördinate of the location
   */
-  pubic int getX(){
+  public int getX(){
+	  return x;
   }
   /**
   searches for the y coördinate of the location
   */
   public int getY(){
+	  return y;
   }
   
   /**
   searches for the z coördinate of the location
   */
   public int getZ(){
+	  return z;
   }
   
   /**
@@ -41,13 +74,15 @@ public class Location {
   @parameter x
   */
   public void setX(int x){
+	  this.x = x;
   }
   
    /**
   set the y cooirdinate of the location
   @parameter y
   */
-  public void setY(int Y){
+  public void setY(int y){
+	  this.y = y;
   }
   
    /*
@@ -55,11 +90,15 @@ public class Location {
   @parameter z
   */
   public void setZ(int z){
+	  this.z = z;
   }
   
   /**
   make a String of the location
   */
-  public String toString(){
-  }
+  @Override
+	public String toString() {
+		return "Location [x=" + x + ", y=" + y + ",z=" + z +"]";
+	}
+
 }
